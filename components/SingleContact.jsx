fee8245e15912b64735ade35167803ce8a99c90d
Nathan Sodja Obodai <sodjanathan@gmail.com>
2022-01-04 22:53:58 +0000

    styling singleContact

--- components/SingleContact.jsx
+++ components/SingleContact.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function SingleContact() {
   return (
-    <div className="contact">
+    <div className="single-contact">
       <div>
         <p>Nathan Sodja</p>
-        <button>Delete</button>
+        <p>0209103354</p>
       </div>
-      <p>0209103354</p>
+      <button className="single-contact__delete">Delete</button>
     </div>
   );
 }

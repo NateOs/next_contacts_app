8c9ec7c889c62b13b6ebc8124ad5fe152c54dbc9
Nathan Sodja Obodai <sodjanathan@gmail.com>
2022-01-04 23:11:44 +0000

    major stylings done

--- components/SingleContact.jsx
+++ components/SingleContact.jsx
@@ -4,8 +4,8 @@ export default function SingleContact() {
   return (
     <div className="single-contact">
       <div>
-        <p>Nathan Sodja</p>
-        <p>0209103354</p>
+        <p className="contact__name">Nathan Sodja</p>
+        <p className="contact__number">0209103354</p>
       </div>
       <button className="single-contact__delete">Delete</button>
     </div>

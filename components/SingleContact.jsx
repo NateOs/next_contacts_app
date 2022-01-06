102fd6eac3fa780bc05be69dd636ff2b98c9e204
Nathan Sodja Obodai <sodjanathan@gmail.com>
2022-01-06 11:51:55 +0000

    added Link, additional styling

--- components/SingleContact.jsx
+++ components/SingleContact.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "next/Link";
+import Link from "next/Link";
 
 export default function SingleContact() {
   return (
     <div className="single-contact">
-      <div>
-        <p className="contact__name">Nathan Sodja</p>
-        <p className="contact__number">0209103354</p>
-      </div>
+      <Link href="/contact/[id]">
+        <div>
+          <p className="contact__name">Nathan Sodja</p>
+          <p className="contact__number">0209103354</p>
+        </div>
+      </Link>
       <button className="single-contact__delete">Delete</button>
     </div>
   );

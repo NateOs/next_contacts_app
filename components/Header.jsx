c3536cb2f6a0cdc066c7b6c04f0d0060fd589fd0
Nathan Sodja Obodai <sodjanathan@gmail.com>
2022-01-06 14:23:36 +0000

    added userRouter to route to form

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link, useRouter } from "next/router";
 import { FaPlus } from "react-icons/fa";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className="header">
       <h1 className="header__title">Contacts</h1>
       <span>
         <FaPlus />
-        <button className="header__add">Add</button>
+        <button className="header__add" onClick={() => router.push("/contact")}>
+          Add
+        </button>
       </span>
     </header>
   );
